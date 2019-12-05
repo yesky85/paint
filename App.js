a0d4103895e729b9b8822703d0f29836ba554764
kec <57247368+yesky85@users.noreply.github.com>
2019-12-05 14:43:34 +0900

    fill canvas

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('colors');
 const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
 canvas.width = 500;
 canvas.height = 500;
 
 ctx.strokeStyle = 'black';
 ctx.lineWidth = 2.5;
+ctx.fillSytle = 'white';
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -33,13 +36,31 @@ function onMouseMove(event) {
 }
 
 function handleColorClick(event) {
-  const color = event.target.style.backgroundColor;
-  ctx.strokeStyle = color;
+  const colorClick = event.target.style.backgroundColor;
+  ctx.strokeStyle = colorClick;
+  ctx.fillStyle = colorClick;
 }
 
-function handleRange(event) {
-  const range = event.target.value;
-  ctx.lineWidth = range;
+function handleRangeInput(event) {
+  const rangeInput = event.target.value;
+  ctx.lineWidth = rangeInput;
+}
+
+function handleModeClick(event) {
+  const modeClick = event.target.innerText;
+  if (modeClick === 'FILL') {
+    mode.innerText = 'PAINT';
+    filling = true;
+  } else {
+    mode.innerText = 'FILL';
+    filling = false;
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
 }
 
 if (canvas) {
@@ -47,6 +68,7 @@ if (canvas) {
   canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
+  canvas.addEventListener('click', handleCanvasClick);
 }
 
 Array.from(colors).forEach(color =>
@@ -54,5 +76,8 @@ Array.from(colors).forEach(color =>
 );
 
 if (range) {
-  range.addEventListener('input', handleRange);
+  range.addEventListener('input', handleRangeInput);
+}
+if (mode) {
+  mode.addEventListener('click', handleModeClick);
 }
